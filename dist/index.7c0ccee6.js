167a956b5f193e46a0a84b371ab3a440aa3584b8
Harshil Tomar <110554721+harshiltomar@users.noreply.github.com>
2023-10-28 13:54:46 +0530

    Add files via upload

---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,25 @@
+// div->div->h1+h2
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child1"
+    }, [
+        React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h2", {}, "I am an h2 tag")
+    ])
+], [
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h2", {}, "I am an h2 tag")
+    ])
+]);
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
